d385c8b9190289fbb060498fd337ea311df12c7c
Jared Furlow <jdf2.furlow@gmail.com>
2021-04-11 21:19:49 -0500

    Various abbreviation changes

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -10,10 +10,19 @@ module.exports = {
   ],
   rules: {
     "prettier/prettier": "error",
+
+    // Style
     indent: ["error", 2],
     quotes: ["error", "double"],
-    "prefer-template": "error",
+    curly: ["error", "multi-line"],
+    "spaced-comment": [1, "always"],
     "import/order": "error",
+    "unicorn/filename-case": "off",
+    "unicorn/prefer-ternary": "off",
+    yoda: "error",
+
+    // Implementations (affects the code beyond just changing spacing/order)
+    "prefer-template": "error",
     "import/no-unresolved": "off", // Really just causes issues, any half decent editor will show errors for this anyways
     "no-var": "error",
     eqeqeq: ["error", "smart"],
@@ -24,33 +33,38 @@ module.exports = {
         ignoreReadBeforeAssign: true,
       },
     ],
-    "spaced-comment": [1, "always"],
-    "unicorn/filename-case": "off",
-    "unicorn/prefer-ternary": "off",
-    yoda: "error",
     "unicorn/prevent-abbreviations": [
       "error",
       {
+        whitelist: {
+          props: true,
+        },
         replacements: {
+          args: false,
           res: {
             result: false,
+          },
+          dir: {
+            direction: false,
           },
         },
       },
     ],
   },
   overrides: [
+    // Config files are normally executed within a Node environment
     {
       files: [
         "./*.config.js",
         "**/.eslintrc.js",
         "**/postcss.config.js",
         "**/tailwind.config.js",
+        "**/windi.config.js",
       ],
       env: {
         node: true,
       },
     },
   ],
-  ignorePatterns: ["node_modules"],
+  ignorePatterns: ["node_modules", "dist/**/*"],
 };
